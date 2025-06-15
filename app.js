1f1eff26d8e458ace5baae8751eecfd998b428b9
zabaoit <nguyengiabao8407@gmail.com>
2025-06-15 09:51:44 +0700

    video #24

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-console.log("Hello World");
-console.log("baobao");
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/zabaoit", (req, res) => {
+  res.send("Zabaoit");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
