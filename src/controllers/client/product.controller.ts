e12ed560c4e0d5ddfb76720e5e8d114b763a0852
zabaoit <nguyengiabao8407@gmail.com>
2025-06-30 20:58:49 +0700

    video #97

--- src/controllers/client/product.controller.ts
+++ src/controllers/client/product.controller.ts
@@ -6,14 +6,46 @@ const getProductPage = (req: Request, res: Response) => {
 };
 
 const getAdminCreateProductPage = (req: Request, res: Response) => {
-  return res.render("admin/product/create.ejs");
+  const errors = [];
+  const oldData = {
+    name: "",
+    price: "",
+    detailDesc: "",
+    shortDesc: "",
+    quantity: "",
+    factory: "",
+    target: "",
+  };
+  return res.render("admin/product/create.ejs", {
+    errors,
+    oldData,
+  });
 };
 
 const postAdminCreateProduct = (req: Request, res: Response) => {
-  const { name } = req.body as TProductSchema;
+  const { name, price, detailDesc, shortDesc, quantity, factory, target } = req.body as TProductSchema;
+  const validate = ProductSchema.safeParse(req.body);
 
+  // err
+  if (!validate.success) {
+    const errorZod = validate.error.issues;
+    const errors = errorZod.map(item => `${item.message} (${item.path[0]})`);
+    const oldData = {
+      name,
+      price,
+      detailDesc,
+      shortDesc,
+      quantity,
+      factory,
+      target,
+    };
+    return res.render("admin/product/create.ejs", {
+      errors,
+      oldData,
+    });
+  }
   try {
-    const result = ProductSchema.parse(req.body);
+    const result = ProductSchema.safeParse(req.body);
     console.log("run ok: ", result);
   } catch (error) {
     console.log(error);
--- src/validation/product.schema.ts
+++ src/validation/product.schema.ts
@@ -2,10 +2,20 @@ import { z } from "zod";
 
 export const ProductSchema = z.object({
   name: z.string().trim().min(1, { message: "Name không được để trống!" }),
-  price: z.number().min(1, { message: "Price tối thiểu là 1!" }),
+  price: z
+    .string()
+    .transform(val => (val === "" ? 0 : Number(val)))
+    .refine(num => num > 0, {
+      message: "Price tối thiểu là 1",
+    }),
   detailDesc: z.string().trim().min(1, { message: "DetailDesc không được để trống!" }),
   shortDesc: z.string().trim().min(1, { message: "ShortDesc không được để trống!" }),
-  quantity: z.number().min(1, { message: "Quantity tối thiểu là 1!" }),
+  quantity: z
+    .string()
+    .transform(val => (val === "" ? 0 : Number(val)))
+    .refine(num => num > 0, {
+      message: "Quantity tối thiểu là 1",
+    }),
   factory: z.string().trim().min(1, { message: "Factory không được để trống!" }),
   target: z.string().trim().min(1, { message: "Target không được để trống!" }),
 });
