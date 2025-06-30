c991aed72add003025466df6bc0a6633763da69f
zabaoit <nguyengiabao8407@gmail.com>
2025-06-30 20:09:40 +0700

    video #96

--- src/controllers/client/product.controller.ts
+++ src/controllers/client/product.controller.ts
@@ -1,4 +1,5 @@
 import { Response, Request } from "express";
+import { ProductSchema, TProductSchema } from "src/validation/product.schema";
 
 const getProductPage = (req: Request, res: Response) => {
   return res.render("client/product/detail.ejs");
@@ -9,7 +10,14 @@ const getAdminCreateProductPage = (req: Request, res: Response) => {
 };
 
 const postAdminCreateProduct = (req: Request, res: Response) => {
-  const { name } = req.body;
+  const { name } = req.body as TProductSchema;
+
+  try {
+    const result = ProductSchema.parse(req.body);
+    console.log("run ok: ", result);
+  } catch (error) {
+    console.log(error);
+  }
   return res.redirect("/admin/product");
 };
 
